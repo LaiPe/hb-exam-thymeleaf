508fc7e781882b5375eb903a23cc3c25404fe347
Léo Peyronnet <peyronnet.leo@gmail.com>
2025-07-03 09:58:25 +0200

    boilerplate initializr

---
+++ src/main/java/com/humanbooster/hbexamthymeleaf/HbExamThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.humanbooster.hbexamthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbExamThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HbExamThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/humanbooster/hbexamthymeleaf/HbExamThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humanbooster.hbexamthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbExamThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
